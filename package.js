fa1892ac501f1826003613a2406a6cd69490658b
CJ Patoilo <cjpatoilo@gmail.com>
2016-01-12 00:42:50 -0300

    preparing for release

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'milligram:milligram-less',
-	version: '0.0.1',
+	version: '1.0.0',
 	summary: 'A minimalist CSS framework on Less version.',
 	git: 'https://github.com/milligram/milligram-less.git',
 	documentation: 'readme.md'
